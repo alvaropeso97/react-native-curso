20b1d41643dcc4edc9b18cb46d5f0fc1d4890797
Álvaro Peso García <alvaro.peso.97@gmail.com>
2018-10-28 11:06:16 +0100

    Contador

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numero: 2
+    }
+  }
+
+  aumentar = () => {
+    this.setState({
+      numero: this.state.numero + 1
+    })
+  }
+
+  disminuir = () => {
+    this.setState({
+      numero: this.state.numero -1
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>¡Hola mundo!</Text>
+        <View style={styles.cajaUno}>
+          <Text>Contador</Text>
+        </View>
+        <View style={styles.cajaDos}>
+          <Text> {this.state.numero} </Text>
+          <Text> this.state.numero </Text>
+        </View>
+        <View style={styles.cajaTres}>
+          <Button 
+          title="Aumentar"
+          onPress={ () => { this.aumentar() } }/>
+          <Button 
+          title="Disminuir"
+          onPress={ () => { this.disminuir() } }/>
+        </View>
       </View>
     );
   }
@@ -15,9 +48,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  cajaUno: {
+    flex: 1,
+    backgroundColor: '#00ff00',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cajaDos: {
+    flex: 3,
+    backgroundColor: '#90EE90',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cajaTres: {
+    flex: 1,
+    backgroundColor: '#006400',
+  }
 });
 
 export default App;
